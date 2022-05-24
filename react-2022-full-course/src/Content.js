ce3ee2e8ab8a58db406c4444e5f05c71bcc128c7
Terabitlab <oscarchb04@gmail.com>
2022-05-24 00:18:03 -0400

    *Add code to manage list

--- react-2022-full-course/src/Content.js
+++ react-2022-full-course/src/Content.js
@@ -1,35 +1,38 @@
 import React, { useState } from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
 
 export const Content = () => {
-    const [name, setName] = useState('Oscar')
-    const [count, setCount] = useState(0)
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            item: 'Item 1',
+            checked: false
+        },
+        {
+            id: 2,
+            item: 'Item 2',
+            checked: false
+        },
+        {
+            id: 3,
+            item: 'Item 3',
+            checked: false
+        },
+    ])
 
-    const handleNameChange = () => {
-        const names = ['fulano', 'fulanito', 'fulanote']
-        const randomName = names[Math.floor(Math.random() * names.length)]
-        setName(randomName)
-    }
 
-    const handleClick = () => {
-        console.log('You clicked me!')
-    }
-
-    const callName = (name) => {
-        console.log(`${name} was called!`)
-    }
-
-    const showEvents = (e) => {
-        console.log(e.target.innerText)
-    }
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {name}!!! and count is {count}
-            </p>
-            <button onClick={handleNameChange} >Click me</button>
-            <button onClick={() => setCount(count + 1)} >Click me to show name</button>
-            <button onClick={(e) => showEvents(e)} >Click me to show events</button>
+            <ul>
+            {items.map(data => (
+                <li className='item' key={data.id}>
+                    <input type='checkbox' checked={data.checked} onChange={() => {}} />
+                    <label>{data.item}</label>
+                    <FaTrashAlt role="button" tabIndex="0" />
+                </li>
+            ))}
+            </ul>
         </main>
     )
 }
