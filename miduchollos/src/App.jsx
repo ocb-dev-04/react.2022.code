b64558942ee4d9b94c2077de3db0f2b3838281cf
Terabitlab <oscarchb04@gmail.com>
2022-06-01 19:00:01 -0400

    *Remove unused project

--- miduchollos/src/App.jsx
+++ miduchollos/src/App.jsx
@@ -2,18 +2,22 @@ import "./App.css";
 import { Route, Routes, Link, useParams } from "react-router-dom";
 
 const Home = () => <h1>Home</h1>;
+
+const Details = () => <h1>Details</h1>;
+
 const SearchPage = () => {
   const tacos = ['cochinita','al pastor', 'al limon', 'asada', 'lengua', 'pollo', 'ranchera', 'veggie']
   return (
     <div>
       <h1>Search</h1>
-      {tacos.map(taco => <li key={taco}><Link to={`/tacos/${taco}/456723`}>{taco}</Link></li>)}
+      {tacos.map(taco => <li key={taco}><Link to={`/tacos/${taco}`}>{taco}</Link></li>)}
     </div>
   )
 }
+
 const Tacos = () => {
-  const { name, id } = useParams()
-  return (<h1>Tacos - ParamValue is: {name} - Id is: {id}</h1>)
+  const { name } = useParams()
+  return (<h1>Tacos - ParamValue is: {name}</h1>)
 };
 
 function App() {
@@ -34,7 +38,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/tacos/:name/:id" element={<Tacos />} />
+        <Route path="/tacos/:name" element={<Tacos />}>
+          <Route path="details" element={<Details />} />
+        </Route>
       </Routes>
     </div>
   );
